d90d2bee3f16d1be99ea2e850562dfc1eb3bec94
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-23 23:54:47 +0200

    feat: create output CV component that represent output of user's CV

---
+++ src/components/OutputCV/OutputCV.tsx
@@ -0,0 +1,9 @@
+import styles from "./OutputCV.module.scss";
+
+export const OutputCV = (): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.document}>CV</div>
+    </div>
+  );
+};
