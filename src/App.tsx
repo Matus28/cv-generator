4cdb245ace2ed6f72d16dc2ee93e7f6b8b10884b
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-23 23:55:13 +0200

    refactor: implements new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import styles from "./App.module.scss";
+import { Accordion } from "./components/Accordion/Accordion";
+import { PersonalInfo } from "./components/Forms/PersonalInfo";
+import { OutputCV } from "./components/OutputCV/OutputCV";
 
 const App = (): JSX.Element => {
-  return <div className={styles.wrapper}>CV Generator</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.leftPanel}>
+        <Accordion title="Personal Information">
+          <PersonalInfo />
+        </Accordion>
+      </div>
+      <div className={styles.rightPanel}>
+        <OutputCV />
+      </div>
+    </div>
+  );
 };
 
 export default App;
