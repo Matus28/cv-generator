90a0e6b7154d007832afb262a73598c1b18db03c
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-24 02:53:59 +0200

    refactor: change display of left panel - flex, and implement accordion for every Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import styles from "./App.module.scss";
 import { Accordion } from "./components/Accordion/Accordion";
+import { Education } from "./components/Forms/Education";
 import { PersonalInfo } from "./components/Forms/PersonalInfo";
+import { Skills } from "./components/Forms/Skills";
+import { WorkExperience } from "./components/Forms/WorkExperience";
 import { OutputCV } from "./components/OutputCV/OutputCV";
 
 const App = (): JSX.Element => {
@@ -9,6 +12,15 @@ const App = (): JSX.Element => {
       <div className={styles.leftPanel}>
         <Accordion title="Personal Information">
           <PersonalInfo />
+        </Accordion>
+        <Accordion title="Education">
+          <Education />
+        </Accordion>
+        <Accordion title="Work Experience">
+          <WorkExperience />
+        </Accordion>
+        <Accordion title="Skills">
+          <Skills />
         </Accordion>
       </div>
       <div className={styles.rightPanel}>
