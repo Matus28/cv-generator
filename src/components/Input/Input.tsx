03067dd398bd63c088fa947762c06619f1a9211c
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-27 02:11:39 +0200

    refactor: implements onChange function for input componets, change input element with TextField elment from MUI, implements new componet PhotoDialog.tsx for showing dialog with input of type=file

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,7 @@
-import { PhotoInput } from "../PhotoInput/PhotoInput";
+import { Dispatch, SetStateAction } from "react";
+import { PhotoDialog } from "../Dialog/PhotoDialog";
 import styles from "./Input.module.scss";
+import { TextField } from "@mui/material";
 
 interface InputProps {
   id: string;
@@ -7,26 +9,37 @@ interface InputProps {
   label: string;
   elementType: string;
   value: string;
+  onChange: Dispatch<SetStateAction<string>>;
   isRequired?: boolean;
 }
 
 export const Input = (props: InputProps): JSX.Element => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    props.onChange(event.currentTarget.value);
+    console.log(event.currentTarget.value);
+  };
+
   return (
     <div className={styles.wrapper}>
       <label>{`${props.label}${props.isRequired ? "*" : ""}`}</label>
       {props.elementType === "input" && (
-        <input
+        <TextField
           id={props.id}
           type={props.type}
           value={props.value}
           required={props.isRequired}
+          onChange={handleChange}
         />
       )}
       {props.elementType === "textarea" && (
         <textarea id={props.id} value={props.value} />
       )}
       {props.elementType === "image" && (
-        <PhotoInput label={props.label} value={props.value} />
+        <PhotoDialog
+          label={props.label}
+          value={props.value}
+          onChange={props.onChange}
+        />
       )}
     </div>
   );
