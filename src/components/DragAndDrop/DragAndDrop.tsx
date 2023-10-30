a5a2cf78487414dbdb622f828e84d42b115276c6
Matus Tatarko <matus.tatarko28@gmail.com>
2023-10-30 23:50:52 +0100

    refactor: edits logic for DragAndDrop component, adds new event handlers

--- src/components/DragAndDrop/DragAndDrop.tsx
+++ src/components/DragAndDrop/DragAndDrop.tsx
@@ -4,17 +4,22 @@ import UserProfile from "../../assets/user-profile-icon.svg";
 import styles from "./DragAndDrop.module.scss";
 
 interface DragAndDropProps {
+  preview: string;
+  value: string;
+  onSetPreview: React.Dispatch<React.SetStateAction<string>>;
   onChange: React.Dispatch<React.SetStateAction<string>>;
   onChangeFile: React.Dispatch<React.SetStateAction<File | null>> | undefined;
   onSetOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const DragAndDrop = (props: DragAndDropProps): JSX.Element => {
-  const [dragActive, setDragActive] = React.useState(false);
+  const [dragActive, setDragActive] = React.useState<boolean>(false);
 
-  const inputRef = React.useRef(null);
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
-  const handleDrag = (event: React.FormEvent<HTMLFormElement>): void => {
+  const handleDrag = (
+    event: React.DragEvent<HTMLFormElement | HTMLDivElement>
+  ): void => {
     event.preventDefault();
     event.stopPropagation();
     if (event.type === "dragenter" || event.type === "dragover") {
@@ -24,47 +29,91 @@ export const DragAndDrop = (props: DragAndDropProps): JSX.Element => {
     }
   };
 
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer.files) {
+      props.onSetPreview(URL.createObjectURL(event.dataTransfer.files[0]));
+    }
+    setDragActive(false);
+  };
+
   const handleChangeFile = (
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
     if (event.target.files && props.onChangeFile) {
       const file = event.target.files[0];
       props.onChangeFile(file);
+      props.onSetPreview(URL.createObjectURL(file));
     }
   };
 
   const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
 
-    const input = event.currentTarget.elements.namedItem(
-      "photo"
-    ) as HTMLInputElement;
-    if (input.files && props.onChangeFile) {
-      props.onChange(URL.createObjectURL(input?.files[0]));
+    // const input = event.currentTarget.elements.namedItem(
+    //   "photo"
+    // ) as HTMLInputElement;
+    // if (input.files && props.onChangeFile) {
+    //   console.log(input?.files);
+    //   props.onChange(URL.createObjectURL(input?.files[0]));
+    // }
+    if (props.preview !== UserProfile) {
+      props.onChange(props.preview);
     }
 
+    props.onSetPreview(UserProfile);
     props.onSetOpen(false);
   };
 
+  const handleButtonClick = (): void => {
+    const input = inputRef.current as HTMLInputElement;
+    input.click();
+  };
+
   return (
-    <form id="photo-form" onDragEnter={handleDrag} onSubmit={onSubmitHandler}>
+    <form
+      id="photo-form"
+      className={styles.form}
+      onDragEnter={handleDrag}
+      onSubmit={onSubmitHandler}
+    >
       <TextField
         className={styles.input}
         id="standard-basic"
         name="photo"
         type="file"
-        ref={inputRef}
+        inputRef={inputRef}
         onChange={handleChangeFile}
         inputProps={{
           accept: "image/*",
         }}
       ></TextField>
-      <Button className={styles.profile}>
-        <img src={UserProfile} alt="Photo of user." />
-        <p>
-          <span>Click</span> or <span>drag</span> your photo here to upload.
-        </p>
-      </Button>
+      <div
+        className={`${styles.profile} ${
+          props.preview !== UserProfile ? styles.uploaded : ""
+        }`}
+      >
+        <Button onClick={handleButtonClick}>
+          <img src={props.preview} alt="Photo of user." />
+          {props.preview === UserProfile && (
+            <p>
+              <span>Click</span>
+              or <span>Drag</span> your photo here to upload.
+            </p>
+          )}
+        </Button>
+      </div>
+      {dragActive && (
+        <div
+          id="drag-file-element"
+          className={styles.placement}
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+        ></div>
+      )}
     </form>
   );
 };
