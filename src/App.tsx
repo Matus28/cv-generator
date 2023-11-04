896b9f569fead1fa4f62baf9d93f19f1ea9cce77
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-04 23:41:17 +0100

    refactor: integrate user options

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import styles from "./App.module.scss";
+import * as React from "react";
 import { Accordion } from "./components/Accordion/Accordion";
-import { Education } from "./components/Forms/Education";
 import { PersonalInfo } from "./components/Forms/PersonalInfo";
 import { Skills } from "./components/Forms/Skills";
 import { WorkExperience } from "./components/Forms/WorkExperience";
@@ -8,16 +7,22 @@ import { OutputCV } from "./components/OutputCV/OutputCV";
 import { BsFillPersonFill, BsFillMouse2Fill } from "react-icons/bs";
 import { MdOutlineWork } from "react-icons/md";
 import { IoIosSchool } from "react-icons/io";
+import { defaultUserData } from "./data/defaultUser";
+import { UserDataType } from "./types/types";
+import { Education } from "./components/Forms/Education";
+import styles from "./App.module.scss";
 
 const App = (): JSX.Element => {
+  const [userData, setUserData] = React.useState<UserDataType>(defaultUserData);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.leftPanel}>
         <Accordion title="Personal Information" icon={<BsFillPersonFill />}>
-          <PersonalInfo />
+          <PersonalInfo data={userData.personalInformation} />
         </Accordion>
         <Accordion title="Education" icon={<IoIosSchool />}>
-          <Education />
+          <Education data={userData.education} />
         </Accordion>
         <Accordion title="Work Experience" icon={<MdOutlineWork />}>
           <WorkExperience />
