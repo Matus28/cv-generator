647f73998674e00a1d10cf1c60b5762eecc30134
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-04 23:41:48 +0100

    refactor: adds new type for Education object

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,4 +1,4 @@
-export interface PersonalInformation {
+export interface PersonalInfoType {
   firstName: string;
   lastName: string;
   photo: string;
@@ -11,4 +11,19 @@ export interface PersonalInformation {
   linkedIn: string;
   github: string;
   description: string;
+}
+
+export interface EducationType {
+  id: string;
+  institution: string;
+  department: string;
+  degreeType: string;
+  city: string;
+  start: number;
+  end: number;
+}
+
+export interface UserDataType {
+  personalInformation: PersonalInfoType;
+  education: EducationType[];
 }
