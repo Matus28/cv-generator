4c7055f86333bc9dc73edc5a253f1b633aff3085
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-09 00:16:31 +0100

    refactor: implements WorkExperience fomr

--- src/components/Forms/WorkExperience.tsx
+++ src/components/Forms/WorkExperience.tsx
@@ -1,3 +1,116 @@
-export const WorkExperience = (): JSX.Element => {
-  return <div>Work Experience</div>;
+import * as React from "react";
+import uniqid from "uniqid";
+import { StyledButton } from "../Button/StyledButton";
+import { WorkExperienceType } from "../../types/types";
+import { WorkExperienceList } from "../WorkExperience/WorkExperienceList";
+import { WorkExperienceForm } from "../WorkExperience/WorkExperienceForm";
+import styles from "./WorkExperience.module.scss";
+
+type Mode = "preview" | "edit" | "add";
+
+type WorkExperienceProps = {
+  data: WorkExperienceType[];
+};
+
+export const WorkExperience = ({ data }: WorkExperienceProps): JSX.Element => {
+  const [workExperience, setWorkExperience] = React.useState<
+    WorkExperienceType[]
+  >(data ?? []);
+  const [editedWorkExperience, setEditedWorkExperience] =
+    React.useState<WorkExperienceType | null>(null);
+  const [mode, setMode] = React.useState<Mode>("preview");
+
+  const handleRemove = (id: string): void => {
+    const newValue = workExperience.filter(
+      (element: WorkExperienceType) => element.id !== id
+    );
+    setWorkExperience(newValue);
+  };
+
+  const handleEdit = (id: string): void => {
+    const selectedWorkExperience = workExperience.filter(
+      (element: WorkExperienceType) => element.id === id
+    )[0];
+    setEditedWorkExperience(selectedWorkExperience);
+    setMode("edit");
+  };
+
+  const handleAdd = (): void => {
+    const id = uniqid();
+
+    setEditedWorkExperience({
+      id: id,
+      employer: "",
+      title: "",
+      startYear: new Date().getFullYear(),
+      startMonth: -1,
+      endYear: new Date().getFullYear(),
+      endMonth: -1,
+      description: "",
+    });
+
+    setMode("add");
+  };
+
+  const handleInputChange = (
+    fieldName: string,
+    value: string | number
+  ): void => {
+    if (editedWorkExperience) {
+      setEditedWorkExperience({
+        ...editedWorkExperience,
+        [fieldName]: value,
+      });
+    }
+  };
+
+  const handleCancel = (): void => {
+    setMode("preview");
+  };
+
+  const handleSave = (): void => {
+    if (mode === "edit") {
+      const newWorkExperienceData = workExperience.map(
+        (element: WorkExperienceType) => {
+          if (element.id === editedWorkExperience?.id) {
+            return editedWorkExperience;
+          }
+          return element;
+        }
+      );
+      setWorkExperience(newWorkExperienceData);
+      setMode("preview");
+      return;
+    } else if (mode === "add" && editedWorkExperience) {
+      const newWorkExperienceData = [...workExperience, editedWorkExperience];
+      setWorkExperience(newWorkExperienceData);
+      setMode("preview");
+      return;
+    }
+
+    setMode("edit");
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {mode === "preview" && (
+        <WorkExperienceList
+          data={workExperience}
+          onEdit={handleEdit}
+          onRemove={handleRemove}
+        />
+      )}
+      {(mode === "edit" || mode === "add") && editedWorkExperience && (
+        <WorkExperienceForm
+          data={editedWorkExperience}
+          cancel={handleCancel}
+          onChange={handleInputChange}
+          onSave={handleSave}
+        />
+      )}
+      {mode === "preview" && (
+        <StyledButton onClick={handleAdd}>Add</StyledButton>
+      )}
+    </div>
+  );
 };
