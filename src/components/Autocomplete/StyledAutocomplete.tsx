bc4c39e9e5446b4e490dea71829ed837434f0655
Matus Tatarko <matus.tatarko28@gmail.com>
2023-11-12 23:22:49 +0100

    feat: adds new component for putting more than one skills on the list

---
+++ src/components/Autocomplete/StyledAutocomplete.tsx
@@ -0,0 +1,37 @@
+import { Autocomplete, Chip, TextField } from "@mui/material";
+// import styles from "./StyledAutocomplete.module.scss";
+
+type StyledAutocomplete = {
+  value: string | string[] | number;
+  onChange: (newValue: string[]) => void;
+};
+
+export const StyledAutocomplete = (props: StyledAutocomplete): JSX.Element => {
+  const skills = props.value as string[];
+
+  const handleChange = (
+    event: React.SyntheticEvent<Element, Event>,
+    newValue: string[]
+  ): void => {
+    props.onChange(newValue);
+  };
+
+  return (
+    <Autocomplete
+      multiple
+      id="tags-filled"
+      options={[]}
+      value={skills}
+      defaultValue={skills}
+      onChange={handleChange}
+      fullWidth
+      freeSolo
+      renderTags={(value: readonly string[], getTagProps) =>
+        value.map((option: string, index: number) => (
+          <Chip variant="outlined" label={option} {...getTagProps({ index })} />
+        ))
+      }
+      renderInput={(params) => <TextField {...params} variant="filled" />}
+    />
+  );
+};
